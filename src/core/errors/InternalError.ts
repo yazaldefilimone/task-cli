a675c30e82b7fc86027fd832883f32ad85b20994
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-20 16:29:33 +0200

    feat: create internal error

---
+++ src/core/errors/InternalError.ts
@@ -0,0 +1,5 @@
+export class InternalError extends Error {
+  constructor() {
+    super(`Internal Error, Please Try Again.`);
+  }
+}
--- src/core/errors/index.ts
+++ src/core/errors/index.ts
@@ -1,2 +1,3 @@
 export * from "./InvalidDescriptionError";
 export * from "./NotFoundTaskError";
+export * from "./InternalError";
