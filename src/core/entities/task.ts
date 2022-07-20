c8917514b319155a8faf2959fa8fd255781c83e0
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-20 16:10:22 +0200

    feat: create entities of task

---
+++ src/core/entities/task.ts
@@ -0,0 +1,23 @@
+export type TaskStorage = {
+  id?: number;
+  description: string;
+  created: string;
+  status: Status;
+  priority: Priority;
+};
+
+export type Task = {
+  id?: number;
+  description: string;
+  age: string;
+  status: Status;
+  priority: Priority;
+};
+
+export type TaskClient = {
+  description: string;
+  priority?: Priority;
+};
+
+type Status = "padding" | "done";
+type Priority = "low" | "high" | "normal";
