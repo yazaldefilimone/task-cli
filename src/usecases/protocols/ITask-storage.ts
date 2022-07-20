026f3cf061c78ade924ccbf227658997c02cabe3
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-20 17:07:59 +0200

    feat: create method param of storage task

--- src/usecases/protocols/ITask-storage.ts
+++ src/usecases/protocols/ITask-storage.ts
@@ -1,8 +1,8 @@
-import { Task } from "@/core/entities";
+import { Task, TaskClient, TaskStorage } from "@/core/entities";
 
 export interface ITaskStorage {
-  create: () => Promise<{ id: string } | Error>;
-  dropOne: () => Promise<{ id: string } | Error>;
-  done: () => Promise<{ id: string } | Error>;
+  create: (data: TaskStorage) => Promise<{ id: string } | Error>;
+  dropOne: (data: { id: string }) => Promise<{ id: string } | Error>;
+  done: (data: { id: string }) => Promise<{ id: string } | Error>;
   getAll: () => Promise<Task[] | Error>;
 }
