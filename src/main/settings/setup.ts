4abfbb03a5f918240499eef1987b8426b12a6170
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-20 17:33:10 +0200

    feat: create program of create task

--- src/main/settings/setup.ts
+++ src/main/settings/setup.ts
@@ -1,21 +1,15 @@
 import packageInfo from "@/../package.json";
+import { CreateTaskCommandFactory } from "@/factories";
 
 import { program } from "commander";
 
-program
-  .name(packageInfo.name)
-  .description(packageInfo.description)
-  .version(packageInfo.version);
+const createTaskCommandFactory = CreateTaskCommandFactory();
+const createTaskBuild = createTaskCommandFactory.build();
 
+program.name(packageInfo.name).description(packageInfo.description).version(packageInfo.version);
 program
-  .command("split")
-  .description("Split a string into substrings and display as an array")
-  .argument("<string>", "string to split")
-  .option("--first", "display just the first substring")
-  .option("-s, --separator <char>", "separator character", ",")
-  .action((str, options) => {
-    const limit = options.first ? 1 : undefined;
-    console.log(str.split(options.separator, limit));
-  });
+  .command(createTaskBuild.command)
+  .description(createTaskBuild.description)
+  .action((str, options) => createTaskBuild.action(str, options));
 
 export default program;
