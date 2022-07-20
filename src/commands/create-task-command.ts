3e00f15b87c5e50c8ea9d3cce5db5cfe4c11beb5
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-20 23:46:11 +0200

    feat: create namespace and rename action method

--- src/commands/create-task-command.ts
+++ src/commands/create-task-command.ts
@@ -1,21 +1,28 @@
-import { ICommandResponse } from "@/commands/protocols";
+import { ICommand, ICommandResponse } from "@/commands/protocols";
 import { ICreateTaskUseCase } from "@/core/usecases";
 
-export class CreateTaskCommand {
+export class CreateTaskCommand implements ICommand{
   private readonly createTaskUseCase: ICreateTaskUseCase;
-
   constructor(createTaskUseCase: ICreateTaskUseCase) {
     this.createTaskUseCase = createTaskUseCase;
   }
 
-  private action() {
-    //  const response = await this.createTaskUseCase.execute()
+  protected action(options: CreateTaskCommand.optionsProps, parameters: { args: string[]}): void | Promise<void>{
+    console.log({options, data:parameters.args})
   }
-  public build(): ICommandResponse {
+
+  public build(): ICommandResponse<typeof this.action>{
     return {
       command: "add",
       description: "create new task",
       action: this.action,
     };
+  }
+}
+
+
+export namespace CreateTaskCommand {
+  export type optionsProps = {
+    priority?: boolean 
   }
 }
