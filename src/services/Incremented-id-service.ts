0fb3ec452911b855f6094e47295b24f2b2e0ec5d
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-20 17:08:34 +0200

    feat: create generation  incremnet task id

---
+++ src/services/Incremented-id-service.ts
@@ -0,0 +1,24 @@
+import { IncrementIdError, InternalError } from "@/core/errors";
+import { Either, left, right } from "@/shared/error-handler/either";
+import { ITaskStorage } from "@/usecases/protocols";
+
+export class IncrementIdService {
+  private readonly taskStorage: ITaskStorage;
+  constructor(taskStorage: ITaskStorage) {
+    this.taskStorage = taskStorage;
+  }
+
+  async id(): Promise<Either<IncrementIdError | InternalError, number>> {
+    try {
+      const all = await this.taskStorage.getAll();
+      if (all instanceof Error) {
+        return left(new IncrementIdError());
+      }
+      const id = all.length;
+
+      return right(id);
+    } catch (error) {
+      return left(new InternalError());
+    }
+  }
+}
---
+++ src/services/index.ts
@@ -0,0 +1,1 @@
+export * from "./Incremented-id-service";
