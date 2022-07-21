b612e34832040c6f986c41a1b60b9a24af072116
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-22 00:05:28 +0200

    fix; change shelljs for fs-promise

--- src/services/unix-shell-service.ts
+++ src/services/unix-shell-service.ts
@@ -1,31 +1,35 @@
-import { writeFile } from "fs/promises";
-import unixShell from 'shelljs';
+import { TaskStorage } from "@/core/entities";
+import { writeFile, readFile } from "fs/promises";
+import unixShell from "shelljs";
 
-export class UnixShellService{
-  private unix: typeof unixShell
-  constructor(unix: typeof unixShell){
-    this.unix = unix
+export class UnixShellService {
+  private unix: typeof unixShell;
+  constructor(unix: typeof unixShell) {
+    this.unix = unix;
   }
-  public async makeDir(filePath:string, fileName:string){
-    this.unix.mkdir('-p', `~/${filePath}/${fileName}`);
+  public async makeDir(filePath: string, fileName: string) {
+    const init = { tasks: [] };
+    const rootPath = filePath;
+    this.unix.mkdir(rootPath);
+    console.log(`${rootPath}/${fileName}`);
+    await writeFile(`${rootPath}/${fileName}`, JSON.stringify(init));
   }
 
-  public async redFile(filePath:string){
-    const file =  this.unix.cat('./package.json');
-    const json = JSON.parse(file);
-    return json.task
+  public async redFile(filePath: string): Promise<TaskStorage[]> {
+    const file = await readFile(filePath);
+    const json = JSON.parse(file as any);
+    console.log(json);
+    return json.tasks;
   }
-  public async writeFile<T = any>(filePath:string, data:T){
+  public async writeFile<T = any>(filePath: string, data: T) {
     try {
-      const file =  this.unix.cat('./package.json');
-      const json = JSON.parse(file);
-      json.task.push(data)
-      await writeFile(filePath, json)
-      return true
-
+      const file = await readFile(filePath);
+      const json = JSON.parse(file as any);
+      json.tasks = data;
+      await writeFile(filePath, JSON.stringify(json));
+      return true;
     } catch (error) {
-      return false
+      return false;
     }
-   
   }
 }
