ab603c163b7609e0b22c303a0a43b99caf5bfeab
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-22 00:06:27 +0200

    feat: create method add task

--- src/services/storage-service.ts
+++ src/services/storage-service.ts
@@ -1,35 +1,50 @@
 import { TaskStorage, Task } from "@/core/entities";
 import { ITaskStorage } from "@/usecases/protocols";
 import { UnixShellService } from "@/services";
-import { InternalError } from "@/core/errors";
+import { InternalError, NotFoundTaskError } from "@/core/errors";
 import { Either, right, left } from "@/shared/error-handler/either";
 
-export class StorageService  implements ITaskStorage{
-  private readonly unixShellService:UnixShellService;
+export class StorageService implements ITaskStorage {
+  private readonly unixShellService: UnixShellService;
   private readonly rootPath: string;
-  private readonly rootFile:string;
-  private readonly root: string
+  private readonly rootFile: string;
+  private readonly root: string;
 
-  constructor(unixShellService:UnixShellService, rootFile:string, rootPath:string, root:string){
+  constructor(unixShellService: UnixShellService, rootFile: string, rootPath: string) {
     this.unixShellService = unixShellService;
     this.rootFile = rootFile;
     this.rootPath = rootPath;
-    this.root = root;
-    this.int(this.rootPath, this.rootFile)
+    this.root = `${rootPath}/${rootFile}`;
+    this.int(this.rootPath, this.rootFile);
   }
 
-  private int(rootPath:string, rootFile:string) {
-    this.unixShellService.makeDir(rootPath, rootFile).then(() => 0).catch()
-  }
-
-  public async create(data: TaskStorage): Promise<Either<Error, { id: number }>>{
-    const response = await this.unixShellService.writeFile<TaskStorage>(this.root, data)
-    if(!response){
-      return left(new InternalError())
+  async dropOne(data: { id: number }): Promise<Either<Error, { id: number }>> {
+    const dataStorage = await this.unixShellService.redFile(this.root);
+    const isExists = dataStorage.find((task) => task?.id === data.id);
+    if (isExists === undefined) {
+      return left(new NotFoundTaskError(String(data.id)));
     }
-    return right({ id: data.id as number })
+    const newsTasks = dataStorage.filter((task) => task.id !== data.id);
+    await this.unixShellService.writeFile<TaskStorage[]>(this.root, newsTasks);
+
+    return right({ id: isExists.id as number });
   }
 
+  private int(rootPath: string, rootFile: string) {
+    this.unixShellService
+      .makeDir(rootPath, rootFile)
+      .then(() => 0)
+      .catch();
+  }
 
+  public async create(data: TaskStorage): Promise<Either<Error, { id: number }>> {
+    const storageTask = await this.unixShellService.redFile(this.root);
+    storageTask.push(data);
 
+    const response = await this.unixShellService.writeFile<TaskStorage[]>(this.root, storageTask);
+    if (!response) {
+      return left(new InternalError());
+    }
+    return right({ id: data.id as number });
+  }
 }
