85faac9490d7fa52d245dd089d7482fa66088235
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-22 00:08:23 +0200

    feat: create factory of createTaskcommand

--- src/factories/TaskFactory.ts
+++ src/factories/TaskFactory.ts
@@ -1,9 +1,14 @@
+import shelljs from "shelljs";
 import { CreateTaskCommand } from "@/commands";
-import { ICreateTaskUseCase } from "@/core/usecases";
-import { IncrementIdService } from "@/services";
+import { IncrementIdService, StorageService, UnixShellService } from "@/services";
 import { CreateTaskUseCase } from "@/usecases";
 
-const taskStorage: any = "not created";
+const user = shelljs.exec("whoami").stdout;
+const rootPath = `/home/${user.trim()}/.task-cli`;
+const rootFile = "task-cli-memory-storage.json";
+
+const unixShellService = new UnixShellService(shelljs);
+const taskStorage = new StorageService(unixShellService, rootFile, rootPath);
 const incrementIdService = new IncrementIdService(taskStorage);
 
 export const CreateTaskCommandFactory = function (): CreateTaskCommand {
