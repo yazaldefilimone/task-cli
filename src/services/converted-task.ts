e260c9e22358831dd4e18bd6fd0f34d87582d23b
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-22 22:58:46 +0200

    refactor: using monent fromNow for compare dates

--- src/services/converted-task.ts
+++ src/services/converted-task.ts
@@ -1,13 +1,10 @@
 import { Task, TaskStorage } from "@/core/entities";
 import moment from "moment";
 export const convertedTask = function (task: TaskStorage): Task {
-  const now = moment().format();
-  const diff = moment(now).diff(moment(task.createdAt));
-
   const newTask = {
     id: task.id,
     description: task.description,
-    age: `${moment.duration(diff).asMinutes()}} seconds`,
+    age: moment(task.createdAt).fromNow(),
     status: task.status,
     priority: task.priority,
   };
