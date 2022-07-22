af3fd3a9ee3e3edbf2dd756b1c088de92c276142
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-22 23:27:55 +0200

    feat; add comands in setup

--- src/main/settings/setup.ts
+++ src/main/settings/setup.ts
@@ -1,16 +1,38 @@
 import packageInfo from "@/../package.json";
-import { CreateTaskCommandFactory } from "@/factories";
+import { CreateTaskCommandFactory, DropTaskCommandFactory, GetAllTaskCommandFactory } from "@/factories";
 
 import { program } from "commander";
 
+//create
 const createTaskCommandFactory = CreateTaskCommandFactory();
 const createTaskBuild = createTaskCommandFactory.build();
+//getAll
+const getAllTaskCommandFactory = GetAllTaskCommandFactory();
+const getAllTaskBuild = getAllTaskCommandFactory.build();
+//drop
+const dropTaskCommandFactory = DropTaskCommandFactory();
+const dropTaskBuild = dropTaskCommandFactory.build();
 
 program.name(packageInfo.name).description(packageInfo.description).version(packageInfo.version);
 program
   .command(createTaskBuild.command)
   .description(createTaskBuild.description)
+  .argument("<to-do>", "name or description of task")
   .option("--priority", "display just the first substring")
   .action((options, parameters) => createTaskCommandFactory.action(options, parameters));
+
+program
+  .command(getAllTaskBuild.command)
+  .description(getAllTaskBuild.description)
+  .option("--all", "show all tasks")
+  .option("--padding", "show all padding status tasks")
+  // .option("-a", "display just the first substring")
+  .action((options, parameters) => getAllTaskCommandFactory.action(options, parameters));
+
+program
+  .command(dropTaskBuild.command)
+  .description(dropTaskBuild.description)
+  .argument("<id>", "id of task")
+  .action((options, parameters) => dropTaskCommandFactory.action(options, parameters));
 
 export default program;
