dc6cca9e50d770e8b1270e43023b40e311436ff0
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-23 23:51:39 +0200

    feat: create task done usecase

---
+++ src/usecases/done-task-usecase.ts
@@ -0,0 +1,34 @@
+import { IDoneTaskUseCase } from "@/core/usecases";
+import { ITaskStorage } from "./protocols";
+import { left, right } from "@/shared/error-handler/either";
+import { InternalError, NotFoundTaskError } from "@/core/errors";
+export class DoneTaskUseCase implements IDoneTaskUseCase {
+  private readonly taskStorage: ITaskStorage;
+  constructor(taskStorage: ITaskStorage) {
+    this.taskStorage = taskStorage;
+  }
+
+  async execute(data: IDoneTaskUseCase.Input): IDoneTaskUseCase.Output {
+    const taskOrError = await this.taskStorage.getAll();
+
+    if (taskOrError.isLeft()) {
+      return left(taskOrError.value);
+    }
+    const tasks = taskOrError.value;
+
+    const task = tasks.find((task) => task.id === data.id);
+
+    if (!task) {
+      return left(new NotFoundTaskError(String(data.id)));
+    }
+    task.status = "done";
+    const storageTask = tasks.filter((task) => task.id !== data.id);
+    storageTask.push(task);
+
+    const response = await this.taskStorage.create(storageTask);
+    if (response.isLeft()) {
+      return left(new InternalError());
+    }
+    return right({ id: task.id as number });
+  }
+}
