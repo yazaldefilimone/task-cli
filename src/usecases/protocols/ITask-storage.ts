b97d216fcde9e63ca6a286ce59c6fb6ba3833525
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-24 00:06:17 +0200

    fix; create create and drop hold task

--- src/usecases/protocols/ITask-storage.ts
+++ src/usecases/protocols/ITask-storage.ts
@@ -2,7 +2,8 @@ import { Task, TaskClient, TaskStorage } from "@/core/entities";
 import { Either } from "@/shared/error-handler/either";
 
 export interface ITaskStorage {
-  create: (data: TaskStorage | TaskStorage[]) => Promise<Either<Error, { id: number } | { ok: boolean }>>;
+  create: (data: TaskStorage) => Promise<Either<Error, { id: number }>>;
+  createDropHold: (data: TaskStorage[]) => Promise<Either<Error, { ok: boolean }>>;
   dropOne: (data: { id: number }) => Promise<Either<Error, { id: number }>>;
   done: (data: { id: number }) => Promise<Either<Error, { id: number }>>;
   getAll: () => Promise<Either<Error, TaskStorage[]>>;
