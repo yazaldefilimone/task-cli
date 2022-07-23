9834e1fbb27b7050050d3080473f30150ed287e8
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-23 23:50:25 +0200

    fix: change taskStoprage

--- src/usecases/protocols/ITask-storage.ts
+++ src/usecases/protocols/ITask-storage.ts
@@ -2,8 +2,8 @@ import { Task, TaskClient, TaskStorage } from "@/core/entities";
 import { Either } from "@/shared/error-handler/either";
 
 export interface ITaskStorage {
-  create: (data: TaskStorage) => Promise<Either<Error, { id: number }>>;
+  create: (data: TaskStorage | TaskStorage[]) => Promise<Either<Error, { id: number } | { ok: boolean }>>;
   dropOne: (data: { id: number }) => Promise<Either<Error, { id: number }>>;
   done: (data: { id: number }) => Promise<Either<Error, { id: number }>>;
-  getAll: () => Promise<Either<Error, Task[]>>;
+  getAll: () => Promise<Either<Error, TaskStorage[]>>;
 }
