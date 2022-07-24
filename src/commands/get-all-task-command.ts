5425e1577e7e8e6348fb486c63fd1befd4cdf257
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-24 20:46:44 +0200

    feat: add async await

--- src/commands/get-all-task-command.ts
+++ src/commands/get-all-task-command.ts
@@ -1,14 +1,24 @@
 import { ICommand, ICommandResponse } from "@/commands/protocols";
 import { IGetAllTaskUseCase } from "@/core/usecases";
+import { UserInterface } from "@/ui";
 
 export class GetAllTaskCommand implements ICommand {
   private readonly getAllTaskUseCase: IGetAllTaskUseCase;
-  constructor(getAllTaskUseCase: IGetAllTaskUseCase) {
+  private readonly userInterface: UserInterface;
+  constructor(getAllTaskUseCase: IGetAllTaskUseCase, userInterface: UserInterface) {
     this.getAllTaskUseCase = getAllTaskUseCase;
+    this.userInterface = userInterface;
   }
 
-  public action(options: GetAllTaskCommand.optionsProps, parameters: { args: string[] }): void | Promise<void> {
-    this.getAllTaskUseCase.execute().then((response) => console.log({ response: response.value }));
+  public async action(options: GetAllTaskCommand.optionsProps, parameters: { args: string[] }): Promise<void> {
+    const response = await this.getAllTaskUseCase.execute();
+    if (response.isLeft()) {
+      return console.log(response.value);
+    }
+
+    const footers = ["total", "27%"];
+    const ui = this.userInterface.list(response.value);
+    return console.log(ui);
   }
 
   public build(): ICommandResponse<typeof this.action> {
