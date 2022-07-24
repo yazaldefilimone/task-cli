09421d7371a31aa2da651f867d23703886650e9c
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-24 20:44:13 +0200

    feat: create table

--- src/factories/TaskFactory.ts
+++ src/factories/TaskFactory.ts
@@ -4,6 +4,8 @@ import { IncrementIdService, StorageService, UnixShellService } from "@/services
 import { CreateTaskUseCase, DropTaskUseCase, GetAllTaskUseCase } from "@/usecases";
 import { DoneTaskCommand } from "@/commands/done-task-command";
 import { DoneTaskUseCase } from "@/usecases/done-task-usecase";
+import { UserInterface } from "@/ui";
+import Table from "easy-table";
 
 const user = shelljs.exec("whoami").stdout;
 const rootPath = `/home/${user.trim()}/.task-cli`;
@@ -12,6 +14,7 @@ const rootFile = "task-cli-memory-storage.json";
 const unixShellService = new UnixShellService(shelljs);
 const taskStorage = new StorageService(unixShellService, rootFile, rootPath);
 const incrementIdService = new IncrementIdService(taskStorage);
+const userInterface = new UserInterface(new Table());
 
 export const CreateTaskCommandFactory = function (): CreateTaskCommand {
   const createTaskUseCase = new CreateTaskUseCase(taskStorage, incrementIdService);
@@ -21,7 +24,7 @@ export const CreateTaskCommandFactory = function (): CreateTaskCommand {
 
 export const GetAllTaskCommandFactory = function (): GetAllTaskCommand {
   const getAllTaskUseCase = new GetAllTaskUseCase(taskStorage);
-  const getAllTaskCommand = new GetAllTaskCommand(getAllTaskUseCase);
+  const getAllTaskCommand = new GetAllTaskCommand(getAllTaskUseCase, userInterface);
   return getAllTaskCommand;
 };
 
