a913e6bead15995bd1c67b5f17b8e978430dd6a8
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-24 20:42:02 +0200

    feat: create ui of terminal

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,4 +1,23 @@
+import { Task } from "@/core/entities";
+import Table from "easy-table";
+
 export class UserInterface {
-  public list() {}
+  private readonly table: Table;
+  constructor(table: Table) {
+    this.table = table;
+  }
+
+  public list(body: Task[]) {
+    const headers = [, "", "", "status", "priority"];
+    body.forEach((task) => {
+      this.table.cell("id", task.id);
+      this.table.cell("description", task.description);
+      this.table.cell("age", task.age);
+      this.table.cell("priority", task.priority);
+      this.table.newRow();
+    });
+
+    return this.table.toString();
+  }
   public notification() {}
 }
